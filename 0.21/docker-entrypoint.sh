75cf0929a57d7add84b336e1fc1c6ef448afe3a0
Jason Livingston <jettoblack@gmail.com>
2021-04-21 17:48:47 -0400

    use uid 1000 and don't recreate data dir

--- 0.21/docker-entrypoint.sh
+++ 0.21/docker-entrypoint.sh
@@ -7,12 +7,22 @@ if [ $(echo "$1" | cut -c1) = "-" ]; then
   set -- bitcoind "$@"
 fi
 
+
 if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bitcoind" ]; then
   mkdir -p "$BITCOIN_DATA"
   chmod 700 "$BITCOIN_DATA"
   chown -R bitcoin "$BITCOIN_DATA"
 
   echo "$0: setting data directory to $BITCOIN_DATA"
+
+  echo "ls -al $BITCOIN_DATA:"
+  ls -al "$BITCOIN_DATA"
+  echo "stat $BITCOIN_DATA:"
+  stat "$BITCOIN_DATA"
+  echo "du $BITCOIN_DATA"
+  du "$BITCOIN_DATA"
+  echo "cat $BITCOIN_DATA/bitcoin.conf:"
+  cat "$BITCOIN_DATA/bitcoin.conf"
 
   set -- "$@" -datadir="$BITCOIN_DATA"
 fi
